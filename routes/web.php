ad348c649854348004e4cfebfa5f625054bb2cd3
Yazji <mw.alyazji@icloud.com>
2026-06-09 23:22:09 +0300

    chore(auth): install laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,9 +8,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function (){
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // Tasks Resources
-    Route::resource('tasks',TaskController::class)->only(['index', 'store', 'update', 'destroy']);
+    // Task Resources
+    Route::resource('tasks', TaskController::class)
+        ->only(['index', 'store', 'update', 'destroy']);
 });
+
+require __DIR__.'/auth.php';

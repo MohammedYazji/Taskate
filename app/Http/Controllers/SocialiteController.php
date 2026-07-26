0aebcbe6348f7fef233941609932216353440adf
Yazji <mw.alyazji@icloud.com>
2026-07-26 09:42:17 +0300

    feat(auth): add Google OAuth login

---
+++ app/Http/Controllers/SocialiteController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialiteController extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function callback()
+    {
+        try {
+            $googleUser = Socialite::driver('google')->user();
+
+            $user = User::where('google_id', $googleUser->getId())->first();
+
+            if (!$user) {
+                $user = User::where('email', $googleUser->getEmail())->first();
+
+                if ($user) {
+                    $user->update([
+                        'google_id' => $googleUser->getId(),
+                        'avatar' => $googleUser->getAvatar(),
+                    ]);
+                } else {
+                    $user = User::create([
+                        'name' => $googleUser->getName(),
+                        'email' => $googleUser->getEmail(),
+                        'google_id' => $googleUser->getId(),
+                        'avatar' => $googleUser->getAvatar(),
+                    ]);
+                }
+            } else {
+                $user->update([
+                    'avatar' => $googleUser->getAvatar(),
+                ]);
+            }
+
+            Auth::login($user, true);
+
+            return redirect()->intended('/dashboard');
+        } catch (\Exception $e) {
+            return redirect()->route('login')->withErrors([
+                'email' => 'Unable to authenticate with Google. Please try again.',
+            ]);
+        }
+    }
+}
